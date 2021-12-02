b628bf174a4054634aeeead0c14a49ecaec38702
FlorinAlexCh <chirut.alex@gmail.com>
2021-12-02 10:25:10 +0200

    assignment2

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scrie textul");
+            string wordCount = Console.ReadLine();
+            string[] count = wordCount.Split(' ');
+            Console.WriteLine("Numarul de cuvinte: " +count.Length);
+        }
+    }
+}
