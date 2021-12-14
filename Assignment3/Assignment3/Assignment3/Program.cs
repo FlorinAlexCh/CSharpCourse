2ba44fd0cf1a6b67a64c9039f5c628472b02a6ad
FlorinAlexCh <chirut.alex@gmail.com>
2021-12-14 18:39:03 +0200

    Assignment3

---
+++ Assignment3/Assignment3/Assignment3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value1 = Console.ReadLine();
+            PrintValue(value1);
+        }
+
+        static void PrintValue(string message)
+        {
+            string result = string.IsNullOrWhiteSpace(message) ? Console.ReadLine() : message;
+            string result1 = string.IsNullOrWhiteSpace(result) ? Console.ReadLine() : result;
+            string result2 = string.IsNullOrWhiteSpace(result1) ? "Nu am continut" : result1;
+            Console.WriteLine(result2);
+        }
+
+    }
+}
