6156c13eb70fae303e6dec08b11c19c2a4dc8a88
FlorinAlexCh <chirut.alex@gmail.com>
2021-12-15 18:53:42 +0200

    Second

--- csharp-statements/Statements/Statements/Program.cs
+++ csharp-statements/Statements/Statements/Program.cs
@@ -6,7 +6,7 @@ namespace Statements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worldd!");
         }
     }
 }
