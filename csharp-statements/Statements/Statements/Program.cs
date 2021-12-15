5ce9efc87638186dda0f741d398ea04fafbdb019
FlorinAlexCh <chirut.alex@gmail.com>
2021-12-15 20:32:19 +0200

    Switch

--- csharp-statements/Statements/Statements/Program.cs
+++ csharp-statements/Statements/Statements/Program.cs
@@ -9,6 +9,28 @@ namespace Statements
             Random randomizer = new Random();
             int i = randomizer.Next(0, 100);
 
+            switch (i % 2)
+            {
+                case 0:
+                   Console.WriteLine($"{i} is Even");
+                    break;
+
+                case 1:
+                    Console.WriteLine($"{i} is Odd");
+                    break;
+
+                default:
+                    Console.WriteLine("Valorea nu e corecta");
+                    break;
+            }
+
+        }
+
+        static void If_Simple()
+        {
+            Random randomizer = new Random();
+            int i = randomizer.Next(0, 100);
+
             bool isEven = i % 2 == 0;
             if (isEven)
             {
@@ -19,5 +41,102 @@ namespace Statements
                 Console.WriteLine($"{i} is not even");
             }
         }
-    }
-}
+
+        static void IfExerciseWithComplexMenu()
+        {
+            Console.WriteLine("Please select an option: ");
+            Console.WriteLine("1. File");
+            Console.WriteLine("2. Edit");
+            Console.WriteLine("3. View");
+            Console.Write("Your option is: ");
+            string level1Option = Console.ReadLine();
+
+            if (string.Equals(level1Option, "1", StringComparison.OrdinalIgnoreCase) ||
+                string.Equals(level1Option, "file", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Please select a sub-option: ");
+                Console.WriteLine("1.1 New");
+                Console.WriteLine("1.2 Open");
+                Console.WriteLine("1.3 Save");
+                Console.Write("Your option is: ");
+                string level2Option = Console.ReadLine();
+                if (string.Equals(level2Option, "1.1", StringComparison.OrdinalIgnoreCase) ||
+                    string.Equals(level2Option, "New", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("You have selected 1.1 NEW");
+                }
+                else if (string.Equals(level2Option, "1.2", StringComparison.OrdinalIgnoreCase) ||
+                        string.Equals(level2Option, "Open", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("You have selected 1.2 Open");
+                }
+                else if (string.Equals(level2Option, "1.3", StringComparison.OrdinalIgnoreCase) ||
+                         string.Equals(level2Option, "Save", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("You have selected 1.3 Save");
+                }
+                Console.WriteLine($"Your Option '{level2Option}' is not valid");
+            }
+            else if (string.Equals(level1Option, "2", StringComparison.OrdinalIgnoreCase) ||
+            string.Equals(level1Option, "Edit", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Please select a sub-option: ");
+                Console.WriteLine("2.1 Cut");
+                Console.WriteLine("2.2 Copy");
+                Console.WriteLine("2.3 Paste");
+                Console.Write("Your option is: ");
+                string level2Option = Console.ReadLine();
+                if (string.Equals(level2Option, "2.1", StringComparison.OrdinalIgnoreCase) ||
+                     string.Equals(level2Option, "Cut", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("You have selected 2.1 Cut");
+                }
+                else if (string.Equals(level2Option, "2.2", StringComparison.OrdinalIgnoreCase) ||
+                        string.Equals(level2Option, "Copy", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("You have selected 2.2 Copy");
+                }
+                else if (string.Equals(level2Option, "2.3", StringComparison.OrdinalIgnoreCase) ||
+                         string.Equals(level2Option, "Paste", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("You have selected 2.3 Paste");
+                }
+                Console.WriteLine($"Your Option '{level2Option}' is not valid");
+            }
+            else if (string.Equals(level1Option, "3", StringComparison.OrdinalIgnoreCase) ||
+            string.Equals(level1Option, "View", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Please select a sub-option: ");
+                Console.WriteLine("3.1 Solution Explorer");
+                Console.WriteLine("3.2 Git changes");
+                Console.WriteLine("3.3 Cloud Explorer");
+                Console.Write("Your option is: ");
+                string level2Option = Console.ReadLine();
+                if (string.Equals(level2Option, "3.1", StringComparison.OrdinalIgnoreCase) ||
+                    string.Equals(level2Option, "Solution Explorer", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("You have selected 3.1 Solution Explorer");
+                }
+                else if (string.Equals(level2Option, "3.2", StringComparison.OrdinalIgnoreCase) ||
+                        string.Equals(level2Option, "Git changes", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("You have selected 3.2 Git changes");
+                }
+                else if (string.Equals(level2Option, "3.3", StringComparison.OrdinalIgnoreCase) ||
+                         string.Equals(level2Option, "Cloud Explorer", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("You have selected 3.3 Cloud Explorer");
+                }
+                else
+                {
+                    Console.WriteLine($"Your Option '{level2Option}' is not valid");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Your Option '{level1Option}' is not valid");
+            }
+        }
+
+     }
+            }
